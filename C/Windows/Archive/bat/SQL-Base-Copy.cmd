63c1203af7ec8757cca598225634c642cecc58de
Oleg Kochkin <39468044+OlegKochkin@users.noreply.github.com>
2023-02-21 01:00:36 +0300

    Update SQL-Base-Copy.cmd

--- C/Windows/Archive/bat/SQL-Base-Copy.cmd
+++ C/Windows/Archive/bat/SQL-Base-Copy.cmd
@@ -1,5 +1,5 @@
 @CD C:\Windows\Archive\bat\SQL-Base-Copy
 @rem mkdir Logs 2> NUL
-@rem powershell .\SQL-Base-Copy.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9| wtee -a Logs\SQL-Base-Copy.log
-@powershell .\SQL-Base-Copy.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9
+@rem powershell .\SQL-Base-Copy.ps1 %* | wtee -a Logs\SQL-Base-Copy.log
+@powershell .\SQL-Base-Copy.ps1 %*
 @pause
